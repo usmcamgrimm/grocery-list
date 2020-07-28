b802d7f8230f3d07a04cd960cc72ef71b0a9d4fa
Adrian Grimm <usmcamgrimm@gmail.com>
2020-07-28 15:42:35 -0400

    Work on this grocery list app

--- script.js
+++ script.js
@@ -1,117 +1,104 @@
-function groceryList() {
-    let list = document.querySelector('#groceryList');
-    let formm = document.querySelector('form');
-    let groceries = document.querySelector('#groceries');
-
-    formm.addEventListener('submit', e => {
-        e.preventDefault();
-        list.innerHTML += `'<li>'${groceries.value}'</li>'`;
-        store();
-        groceries.value = '';
-    }, false)
-
-    list.addEventListener('click', e => {
-        let t = e.target;
-        if(t.classList.contains('checked')) {
-            t.parentNode.removeChild(t);
-        } else {
-            t.classList.add('checked');
+let grocery = {
+    data: [],
+    load: function() {
+        //load list from local storage
+        if (localStorage.list == undefined) {
+            localStorage.list = "[]";
         }
-        store();
-    }, false)
+        grocery.data = JSON.parse(localStorage.list);
+        grocery.list();
+    },
 
-    function store() {
-        window.localStorage.myGroceries = list.innerHTML;
-    }
+    save: function () {
+        localStorage.list = JSON.stringify(grocery.data);
+        grocery.list();
+    },
 
-    function getValues() {
-        let storedValues = window.localStorage.myGroceries;
-        if(!storedValues) {
-            list.innerHTML = '';
-        } else {
-            list.innerHTML = storedValues;
+    list: function () {
+        let container = document.getElementById("groceries");
+        container.innerHTML = "";
+
+        if (grocery.data.length > 0) {
+            let row = "", e = "";
+            for (var key in grocery.data) {
+                row = document.createElement("div");
+                row.classList.add("clearfix");
+                row.dataset.id = key;
+
+                e = document.createElement("div");
+                e.classList.add("item");
+                if (grocery.data[key][1] == 1) {
+                    e.classList.add("done");
+                }
+                if (grocery.data[key][1] == 2) {
+                    e.classList.add("cx");
+                }
+                e.innerHTML = grocery.data[key][0];
+                row.appendChild(e);
+
+                e = document.createElement("input");
+                e.setAttribute("type", "button");
+                e.value = "\u2716";
+                e.classList.add("bdel");
+                e.addEventListener("click", function () {
+                    grocery.status(this, 1);
+                });
+                row.appendChild(e);
+
+                e = document.createElement("input");
+                e.setAttribute("type", "button");
+                e.value = "\u2714";
+                e.classList.add("bchecked");
+                e.addEventListener("click", function () {
+                grocery.status(this, 1);
+                });
+                row.appendChild(e);
+
+                container.appendChild(row);
+            }
+        }
+    },
+
+    add: function () {
+        //Add a new item
+        grocery.data.push([
+            document.getElementById("groceryAdd").value, 0
+        ]);
+        document.getElementById("groceryAdd").value="";
+        grocery.save();
+    },
+
+    status: function (e, stat) {
+        let parent = e.parentElement;
+        grocery.data[parent.dataset.id][1] = stat;
+        grocery.save();
+    },
+
+    del: function (type) {
+        if (confirm("Clear List?")) {
+            //delete all items
+            if (type == 0) {
+                grocery.data = [];
+                grocery.save();
+            }
+            else {
+                grocery.data = grocery.data.filter(row => row[1]==0);
+                grocery.save();
+            }
         }
     }
-    getValues;
-}
+};
 
-/*
-let groceryItems = localStorage.getItem('groceries') ? JSON.parse(localStorage.getItem('groceries')) : [];
-
-localStorage.setItem('groceries', JSON.stringify(groceryItems));
-const data = JSON.parse(localStorage.getItem('groceries'));
-
-function renderGrocery(grocery) {
-    const list = document.querySelector('.js-grocery-list');
-    list.insertAdjacentHTML('beforeend', `
-        <li class="grocery-item" data-key="${grocery.id}">
-            <input id="${grocery.id}" type="checkbox" />
-            <label for="${grocery.id}" class="tick js-tick"></label>
-            <span>${grocery.text}</span>
-            <button class="delete-grocery js-delete-grocery">
-                <svg><use href="#delete-icon"></use></svg>
-            </button>
-        </li>
-    `);
-}
-
-function addGrocery(text) {
-    const grocery = {
-        text,
-        checked: false,
-        id: Date.now(),
-    };
-
-    groceryItems.push(grocery);
-    renderGrocery(grocery);
-    localStorage.setItem('groceries', JSON.stringify(groceryItems))
-}
-
-function toggleDone(key) {
-    const index = groceryItems.findIndex(item => item.id === Number(key));
-    groceryItems[index].checked = !groceryItems[index].checked;
-
-    const item = document.querySelector(`[data-key='${key}']`);
-    if (groceryItems[index].checked) {
-        item.classList.add('done');
-    } else {
-        item.classList.remove('done');
-    }
-}
-
-function deleteGrocery(key) {
-    groceryItems = groceryItems.filter(item => item.id !== Number(key));
-    const item = document.querySelector(`[data-key='${key}']`);
-    item.remove();
-
-    const list = document.querySelector('.js-grocery-list');
-    if (groceryItems.length === 0) list.innerHTML = '';
-}
-
-const form = document.querySelector('.js-form');
-form.addEventListener('submit', e => {
-    e.preventDefault();
-    const input = document.querySelector('.js-grocery-input');
-
-    const text = input.value.trim();
-    if (text !== '') {
-        addGrocery(text);
-        input.value = '';
-        input.focus();
-    }
-});
-
-const list = document.querySelector('.js-grocery-list');
-list.addEventListener('click', e => {
-    if (e.target.classList.contains('js-tick')) {
-        const itemKey = e.target.parentElement.dataset.key;
-        localStorage.removeItem(`${key}`);
-        toggleDone(itemKey);
-    }
-
-    if (e.target.classList.contains('js-delete-grocery')) {
-        const itemKey = event.target.parentElement.dataset.key;
-        deleteGrocery(itemKey);
-    }
-});
-*/
+window.addEventListener("load", function () {
+    document.getElementById("clearList").addEventListener("click", function () {
+        grocery.del(0);
+    });
+    document.getElementById("clearChecked").addEventListener("click", function () {
+        grocery.del(1);
+    });
+    document.getElementById("groceryForm").addEventListener("submit", function (evt) {
+        evt.preventDefault();
+        grocery.add();
+    });
+    grocery.load();
+})
